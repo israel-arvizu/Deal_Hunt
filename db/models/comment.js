467b1253f60898a5c7b6a9fa034767680f58d1ad
uki <ukiworks034@gmail.com>
2022-05-09 17:13:51 -0400

    updated comment model and migration

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,10 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Comment = sequelize.define('Comment', {
-    id: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    reviewId: DataTypes.INTEGER,
-    content: DataTypes.STRING
+    userId: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+    },
+    reviewId: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+    },
+    content: {
+     type: DataTypes.STRING(350),
+     allowNull: false,
+    },
   }, {});
   Comment.associate = function(models) {
     // associations can be defined here

fdc7401ef131799fc93ea98e7fef49712d93990b
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-10 09:45:23 -0700

    Route Testing

--- db/seeders/20220509225153-reviews.js
+++ db/seeders/20220509225153-reviews.js
@@ -13,7 +13,7 @@ module.exports = {
       }], {});
     */
     return queryInterface.bulkInsert('Reviews', [
-      { albumId: 6, userId: 3, content: "On their new album, the artist turns their steady gaze towards the wreckage of the last few years and still finds a way to prioritize compassion.", createdAt: '2019-04-12', updatedAt: '2019-04-12' },
+      { albumId: 1, userId: 3, content: "On their new album, the artist turns their steady gaze towards the wreckage of the last few years and still finds a way to prioritize compassion.", createdAt: '2019-04-12', updatedAt: '2019-04-12' },
       { albumId: 2, userId: 1, content: "This album is, at its heart, an album about the freedom afforded by the open road. But where world' music's stuck on back roads, this album speeds down the interstate.", createdAt: '2012-01-12', updatedAt: '2012-01-12' },
       { albumId: 3, userId: 2, content: "The songwriter's new album interrogates what it means for their self-image to be centered on their art, while grappling with the way capitalist forces threaten to mute its radical possibilities.", createdAt: '2019-05-21', updatedAt: '2019-05-21' },
       { albumId: 4, userId: 4, content: "On the largely wordless everything perfect is already here, the composer lets us experience the world through their ears with field recordings, strings and a little tenderness.", createdAt: '2022-01-17', updatedAt: '2022-01-17' },
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,107 @@
 var express = require('express');
 const { csrfProtection, asyncHandler } = require('./utils');
+const { check, validationResult} = require('express-validator');
 const db = require('../db/models');
 
 
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  
-});
+// router.get('/', function(req, res, next) {
+
+// });
+
+router.get('/signup', csrfProtection, asyncHandler(async (req, res, next) => {
+//     const user = db.User.build();
+
+//     res.render('signup', {
+//         csrfToken: req.csrfToken(),
+//         errors: [],
+//         user,
+//         title: "Register"
+res.send("Hello World")
+    //  });
+
+}));
+
+// const signUpValidator = [
+//     check('firstName')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a value for First Name')
+//     .isLength({ max: 50 })
+//     .withMessage('First Name must not be more than 50 characters long'),
+//   check('lastName')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a value for Last Name')
+//     .isLength({ max: 50 })
+//     .withMessage('Last Name must not be more than 50 characters long'),
+//   check('email')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a value for Email Address')
+//     .isLength({ max: 255 })
+//     .withMessage('Email Address must not be more than 255 characters long')
+//     .isEmail()
+//     .withMessage('Email Address is not a valid email')
+//     .custom((value) => {
+//       return db.User.findOne({ where: { emailAddress: value } })
+//         .then((user) => {
+//           if (user) {
+//             return Promise.reject('The provided Email Address is already in use by another account');
+//           }
+//         });
+//     }),
+//   check('hashedPassword')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a value for Password')
+//     .isLength({ max: 50 })
+//     .withMessage('Password must not be more than 50 characters long')
+//     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+//     .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+//   check('confirmPassword')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Please provide a value for Confirm Password')
+//     .isLength({ max: 50 })
+//     .withMessage('Confirm Password must not be more than 50 characters long')
+//     .custom((value, { req }) => {
+//       if (value !== req.body.password) {
+//         throw new Error('Confirm Password does not match Password');
+//       }
+//       return true;
+//     }),
+// ]
+
+
+
+// router.post('/signup', csrfProtection, signUpValidator, asyncHandler(async(req, res, next) =>{
+//     const {
+//         firstName,
+//         lastName,
+//         email,
+//         hashedPassword
+//     } = req.body
+
+//     const user = db.User.build({
+//         firstName,
+//         lastName,
+//         email,
+//         hashedPassword
+//     })
+
+//     const validatorErrors = validationResult(req);
+
+//     if(validatorErrors.isEmpty()){
+//         await user.save();
+//         res.redirect('/');
+//     }else {
+//         const errors = validatorErrors.array().map((error) => error.msg);
+//         res.render('signup', {
+//             title: "Register",
+//             user,
+//             errors,
+//             csrfToken: req.csrfToken()
+//         })
+//     }
+// }));
+
 
 module.exports = router;

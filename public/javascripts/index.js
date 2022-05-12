c03895f5703126e0a94f2d70aa7de45297a20c95
uki <ukiworks034@gmail.com>
2022-05-12 17:05:36 -0400

    started on demo user

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,28 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
+
+    const demoButton = document.getElementById('demo');
+    console.log(demoButton);
+
+    demoButton.addEventListener('click', async(e) => {
+        e.preventDefault();
+        console.log('in the event listener')
+
+      const res = await fetch('/users/signin', {
+          method: 'GET'
+      })
+
+    //   const data = await res.json();
+      console.log(res);
+      const demoToken = document.getElementsByClassName('token');
+      console.log(demoToken)
+        await fetch('/users/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: {
+                email: 'demo@user.com',
+                hashedPassword: 'Demo11!'
+            }
+        })
+    })
 })

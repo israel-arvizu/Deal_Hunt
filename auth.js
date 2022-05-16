54c3526c46ad5d505f52ff348b9bb9dcb6496120
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-15 21:03:08 -0700

    fix-sixteen

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const db = require('./db/models');
 
 const signinUser = (req, res, user) => {
     req.session.auth = {userId: user.id};
+    return res.redirect('/');
 }
 const signoutUser = (req, res) => {
     delete req.session.auth;

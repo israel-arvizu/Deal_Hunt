b6c051e89a1f4ce3016e731529f8899eb479790b
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-15 19:15:56 -0700

    test 2

--- routes/index.js
+++ routes/index.js
@@ -66,17 +66,17 @@ router.get('/albums', csrfProtection,  asyncHandler(async (req, res) =>{
 
 router.get('/about', csrfProtection, asyncHandler(async(req, res) => {
   console.log('ENTERED ABOUT')
-  if(req.session.auth){
-    const { userId } = req.session.auth
-    const loggedInUser = await db.User.findByPk(userId)
-    console.log(loggedInUser);
-    res.render('about', {
-      Title: 'About',
-      userId,
-      loggedInUser,
-      csrfToken: req.csrfToken()
-    })
-  }
+  // if(req.session.auth){
+  //   const { userId } = req.session.auth
+  //   const loggedInUser = await db.User.findByPk(userId)
+  //   console.log(loggedInUser);
+  //   res.render('about', {
+  //     Title: 'About',
+  //     userId,
+  //     loggedInUser,
+  //     csrfToken: req.csrfToken()
+  //   })
+  // }
 
   res.render('guest-about', {
     Title: 'About',
